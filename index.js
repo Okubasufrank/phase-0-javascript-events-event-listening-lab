0c3c73bf02a7a6825ef532eac414f4fa2c50397b
Okubasufrank <franklineokubasu51@gmail.com>
2022-06-23 14:27:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+        clickAlert();
+    });
 }
+
+
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
